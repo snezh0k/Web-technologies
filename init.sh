967135a0798bb48ab155b3d788607a24253e8037
snezh0k <korotaev.al.ev@gmail.com>
2016-11-23 22:37:04 +0300

    true

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/nginx.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
