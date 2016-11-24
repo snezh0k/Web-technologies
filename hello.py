94fc184f2a1fa42298ad16614aa0bb5467d9be0e
snezh0k <korotaev.al.ev@gmail.com>
2016-11-24 04:34:52 +0300

    hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+        ]
+    body = 'Hello, world!'
+    start_response(status, headers)
+    return [ body ]
