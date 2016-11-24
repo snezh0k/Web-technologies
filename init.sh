93e991fcdb078f70c1bd0c734eb92c0abd337309
snezh0k <korotaev.al.ev@gmail.com>
2016-11-24 04:37:09 +0300

    also

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo rm /etc/nginx/nginx.conf
+sudo rm /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart /etc/gunicorn.d/hello.py
