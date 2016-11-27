ced84487525a566f668a6e89dc6bf4128ce9147a
snezh0k <korotaev.al.ev@gmail.com>
2016-11-27 09:48:20 +0300

    bI

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default = 0)
     author = models.OneToOneField(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='question_like_author', default='x')
     def __unicode__(self):
